bae31964594bab8e700caca379d63574e1730c70
Richard Hsu <rhsu@gwu.edu>
2016-05-23 17:41:44 -0400

    Added json with index

--- app.rb
+++ app.rb
@@ -34,13 +34,46 @@ get '/cake' do
  # 	File.read("cake.list")
 end 
 
+
+
+
 get '/json' do
 	#erb :index, :locals => {:name => params[:name]}}
 	arr = IO.readlines("cake.list")
-  	puts arr
-  	puts "JSON"
-	puts JSON.generate(arr)
+  	#puts arr
+  	#puts "JSON"
+	#puts JSON.generate(arr)
 	@json = JSON.generate(arr)
+
+	dictionary = []
+
+	cake = []
+
+	hash = {}
+
+	arr.each_with_index {|val, index| puts "#{val} => #{index}" 
+
+		cake.push index
+		cake.push val
+		#puts cake
+		dictionary.push cake
+
+		puts hash
+		hash[index.to_s] = val
+
+		cake = []
+	}
+
+	puts "---Hash---"
+	puts hash
+
+	puts "---Dictionary---"
+	puts dictionary
+	@dictionary = dictionary
+
+	@json_with_numbers = JSON.generate(dictionary)
+	@hash = hash 
+
 
 	erb :json
 end
