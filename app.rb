a80a0fa34bfce4b28f0e7a2dcf4f891bc0113c02
Richard Hsu <rhsu@gwu.edu>
2016-05-23 11:54:04 -0400

    Got cake list to show up

--- app.rb
+++ app.rb
@@ -1,5 +1,36 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world"
-end
+  "Hello, world again!"
+
+
+
+# File.open("cake.list", "r") do
+
+#arr = IO.readlines("cake.list") 
+#puts arr[0]
+
+end 
+
+File.open("cake.list", "r") do |f|
+	f.each_line do |line|
+		line + ""
+		end 
+	end 
+
+
+get '/cake' do
+	#erb :index, :locals => {:name => params[:name]}}
+	@name = "Richard"
+	@arr = IO.readlines("cake.list")
+	erb :index
+	# File.open("cake.list", "r") do |f|
+	# f.each_line do |line|
+	# 	line + ""
+	# 	end 
+	# end 
+ # 	File.read("cake.list")
+end 
+
+
+
