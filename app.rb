4264c019c8bd2b196f2ccd49d1c72ab748c0d94e
Richard Hsu <rhsu@gwu.edu>
2016-05-24 22:14:33 -0400

    Set up Postgres

--- app.rb
+++ app.rb
@@ -1,5 +1,91 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require './models/model' 
 
 get '/' do
-  "Hello, world"
+  "Hello, world again!"
+
+end 
+
+get '/test' do
+	erb :index
 end
+
+post '/submit' do
+	@model = Model.new(params[:model])
+	if @model.save
+		redirect '/models'
+	else
+		"Sorry, there was an error!"
+	end
+end
+
+get '/models' do
+	@models = Model.all
+	erb :models
+end
+
+get '/cake' do
+	#erb :index, :locals => {:name => params[:name]}}
+	@name = "Richard"
+	@arr = IO.readlines("cake.list")
+	#@arr = JSON.parse(arr)
+	erb :cake
+	# File.open("cake.list", "r") do |f|
+	# f.each_line do |line|
+	# 	line + ""
+	# 	end 
+	# end 
+ # 	File.read("cake.list")
+end 
+
+
+
+
+get '/json' do
+	#erb :index, :locals => {:name => params[:name]}}
+	arr = IO.readlines("cake.list")
+  	#puts arr
+  	#puts "JSON"
+	#puts JSON.generate(arr)
+	@json = JSON.generate(arr)
+
+	dictionary = []
+
+	cake = []
+
+	hash = {}
+
+	arr.each_with_index {|val, index| puts "#{val} => #{index}" 
+
+		cake.push index
+		cake.push val
+		#puts cake
+		dictionary.push cake
+
+		puts hash
+		hash[index.to_s] = val
+
+		cake = []
+	}
+
+	puts "---Hash---"
+	puts hash
+
+	puts "---Dictionary---"
+	puts dictionary
+	@dictionary = dictionary
+
+	@json_with_numbers = JSON.generate(dictionary)
+	@hash = hash 
+
+
+	erb :json
+end
+
+
+
+
+
+
